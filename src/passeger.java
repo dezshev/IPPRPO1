c91e2d552ec764769891fb5b399ec6bc131631ac
dezshev <dezshev@gmail.com>
2025-10-07 12:45:00 +0300

    Update passeger.java

--- src/passeger.java
+++ src/passeger.java
@@ -1,1 +1,24 @@
 
+// Passenger.java
+public class Passenger {
+    private String name;
+    private int age;
+
+    public Passenger(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + age + " лет)";
+    }
+}
