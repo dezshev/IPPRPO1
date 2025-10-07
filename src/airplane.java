c90fc3fcd6b2e47a90dd24a035a146ecf4344493
dezshev <dezshev@gmail.com>
2025-10-07 12:45:20 +0300

    Update airplane.java

--- src/airplane.java
+++ src/airplane.java
@@ -1,1 +1,23 @@
+// Airplane.java
+public class Airplane {
+    private String model;
+    private int capacity;
 
+    public Airplane(String model, int capacity) {
+        this.model = model;
+        this.capacity = capacity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    @Override
+    public String toString() {
+        return model + " (вместимость: " + capacity + ")";
+    }
+}
