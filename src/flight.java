c7c9c20005b7aa94706aa4dda6eb80632eb7bd11
dezshev <dezshev@gmail.com>
2025-10-07 12:45:35 +0300

    Update flight.java

--- src/flight.java
+++ src/flight.java
@@ -1,1 +1,37 @@
+// Flight.java
+import java.util.List;
 
+public class Flight {
+    private String flightNumber;
+    private String destination;
+    private Airplane airplane;
+    private List<Passenger> passengers;
+
+    public Flight(String flightNumber, String destination, Airplane airplane, List<Passenger> passengers) {
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.airplane = airplane;
+        this.passengers = passengers;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public Airplane getAirplane() {
+        return airplane;
+    }
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    @Override
+    public String toString() {
+        return "Рейс " + flightNumber + " → " + destination + " | " + airplane.getModel();
+    }
+}
